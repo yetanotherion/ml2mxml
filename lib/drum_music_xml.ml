39ea4e0d4a7c8ed4b103d927a447e599ca1eaf4e
Ion Alberdi <nolaridebi@gmail.com>
2015-07-14 21:33:37 +0200

    add license

--- lib/drum_music_xml.ml
+++ lib/drum_music_xml.ml
@@ -1,3 +1,18 @@
+(**************************************************************************)
+(*  Copyright 2015, Ion Alberdi <nolaridebi at gmail.com>                 *)
+(*                                                                        *)
+(*  Licensed under the Apache License, Version 2.0 (the "License");       *)
+(*  you may not use this file except in compliance with the License.      *)
+(*  You may obtain a copy of the License at                               *)
+(*                                                                        *)
+(*      http://www.apache.org/licenses/LICENSE-2.0                        *)
+(*                                                                        *)
+(*  Unless required by applicable law or agreed to in writing, software   *)
+(*  distributed under the License is distributed on an "AS IS" BASIS,     *)
+(*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or       *)
+(*  implied.  See the License for the specific language governing         *)
+(*  permissions and limitations under the License.                        *)
+(**************************************************************************)
 let drum_element_to_mxml_param x =
   (* this is from guitar pro 6 music xml
      that seems buggy, as the same notation is given
